61ecba3fab8c4d15998161c51a7ff9942a090139
Julia Dubas <uliadubas94@gmail.com>
2022-07-22 13:42:24 +0300

    Add project files.

---
+++ ConsistentArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsistentArray
+{
+    class Program
+    {
+        static int? FirstNonConsecutiveElement(int[] Array)
+        {
+
+           int? value = null;
+
+
+            for(int i = 1; i < Array.Length; i++)
+            {
+                if (Array[i] - 1 != Array[i - 1])
+                {
+
+                    value = Array[i];
+                    break;
+                }
+               
+            }
+            return value;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 1, 2, 3, 4, 6, 7 };
+
+
+            Console.WriteLine(FirstNonConsecutiveElement(myArray));
+
+            Console.ReadLine();
+        }
+    }
+}
